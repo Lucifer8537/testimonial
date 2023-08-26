d5d4ec8f17ee4fa466c164c5d09af3d483268de2
navodit <navodit50@gmail.com>
2023-08-26 18:24:11 +0530

    first push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css', './mobile.component.css'],
 })
-export class AppComponent {
-  title = 'testimonial';
+export class AppComponent implements OnInit {
+  width!: number;
+  mobileView!: boolean;
+
+  @HostListener('window:resize', ['$event'])
+  onResize = () => {
+    this.width = window.innerWidth;
+    this.mobileView = this.width < 679;
+  };
+
+  ngOnInit(): void {
+    this.onResize();
+  }
 }
